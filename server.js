2147dd87d6b50678e03ba17bbfaab8f826313b3f
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-01-19 15:18:56 +0530

    set up ejs and other things

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+const morgan = require('morgan');
+const path = require('path');
+
+//middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan('dev'));
+
+//setting up ejs
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log('App running at port', port);
+});
